a7edaedd95bc8814cb46ea9ef730faf50739b3ff
lezhnina-anna <Ankemova@gmail.com>
2018-03-15 22:30:48 +0300

    исправлены замечания

--- Lab01/multmatrix/test.bat
+++ Lab01/multmatrix/test.bat
@@ -30,7 +30,7 @@ if ERRORLEVEL 1 goto err
 rem несуществующий входной файл
 %PROGRAM% non-existing-file.txt input.txt > output.txt
 if NOT ERRORLEVEL 1 goto err
-fc output.txt InvalidOutput.txt
+fc output.txt FileError.txt
 if ERRORLEVEL 1 goto err
 
 echo Program testing succeeded
